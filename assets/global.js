e159ad19284c64768aed2a5c66715b1655bb440c
jeremy24k <jeremysanchezsoto@gamil.com>
2023-04-05 12:27:58 -0400

    add-sections

--- assets/global.js
+++ assets/global.js
@@ -1063,3 +1063,58 @@ class ProductRecommendations extends HTMLElement {
 }
 
 customElements.define('product-recommendations', ProductRecommendations);
+
+//-----------slider-section-tasty-----------
+const sliderTasty = new Swiper('.container-slider-tasty', {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+
+  breakpoints: {
+      310: {
+          slidesPerView: 1,
+          spaceBetween: 20
+      },
+
+      500: {
+          slidesPerView: 2,
+          spaceBetween: 30
+      },
+
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+      },
+  }
+});
+
+//-----------slider-section-tasty-----------
+const sliderChick = new Swiper('.slider-section-chick', {
+  slidesPerView: 2.5,
+  loop: true,
+  spaceBetween: 135,
+  centeredSlides: 'true',
+  
+  breakpoints: {
+      310: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+      },
+
+    768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+      },
+
+    1200: {
+        slidesPerView: 2.5,
+        spaceBetween: 135,
+    },
+  }
+});
+
